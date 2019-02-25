a07ca11cdfe1e27341b7769bf58134019526f4d5
cj summers <cjsummers003@hotmail.com>
2019-02-25 16:04:23 -0500

    initial update

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+// Templated Friends Data
+var friendsData = [
+    {
+      name: "CJ Summers",
+      photo: "https://scontent.ftpa1-2.fna.fbcdn.net/v/t1.0-9/223320_200192846689295_1194050_n.jpg?_nc_cat=111&_nc_eui2=AeHxL79MtJ1Dh2BffdS6sRldy7CU1Y6lHKv5uMt1CGtr2QTGajMAUQz6LqbnVF7MGJ_Yzp8LjvcOcfjIXzFiK2feoFp9kk3iP0eCs7p9VLjMRg&_nc_ht=scontent.ftpa1-2.fna&oh=9bb860e469bc0b9603a136c7a32a19eb&oe=5CE6418B",
+      scores: [3, 4, 3, 3, 4, 4, 4, 1, 5, 3]
+    },
+    {
+      name: "Darth Vader",
+      photo:"https://lumiere-a.akamaihd.net/v1/images/Darth-Vader_6bda9114.jpeg?region=0%2C23%2C1400%2C785&width=960",
+      scores: [2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+    },
+    {
+      name: "Captain America",
+      photo:"https://cdn.britannica.com/s:500x350/30/182830-004-61C7794A.jpg",
+      scores: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
+    },
+    {
+      name: "Darth Revan",
+      photo:"https://qph.fs.quoracdn.net/main-qimg-f2886c143c8b90b1472cbc1b20902bf3.webp",
+      scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+    },
+    {
+      name: "Pac-Man",
+      photo:"https://www.smashbros.com/wiiu-3ds/sp/us/images/character/pac-man/illust.jpg",
+      scores: [5, 4, 4, 4, 4, 3, 2, 1, 5, 5]
+    },
+  ];
+  
+  // Export to API Route
+  module.exports = friendsData;
